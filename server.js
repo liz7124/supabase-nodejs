ea8ede14258118a3f46f3a80cb013e70d1ed5b1d
Elizabeth Nathania W <liza_1805@hotmail.com>
2025-11-25 15:49:12 +0700

    update index, script, server

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import studentRoutes from './src/routes/studentRoutes.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const PORT = 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public'))) //set static path ke public
 
 app.use('/api/students', studentRoutes)
 
 app.get('/', (req, res) => {
-  res.send('Supabase + Express API is running')
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.listen(PORT, () => {
