e7cddbaee6f8fb943de41bd69ee9b82ca75ba191
Elizabeth Nathania W <liza_1805@hotmail.com>
2025-12-10 08:20:27 +0700

    public + db.js

--- src/db.js
+++ src/db.js
@@ -3,6 +3,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+//Authentication
+export const supabaseAuth = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
+//CRUD + Realtime
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY, {
